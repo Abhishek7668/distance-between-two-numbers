1c10f8a532714e63ec3b997317677e296054e5b6
Abhishek7668 <92740857+Abhishek7668@users.noreply.github.com>
2021-12-13 22:59:10 +0530

    Add files via upload

---
+++ distance bw two point.py
@@ -0,0 +1,8 @@
+a = int ( input( " enter a distance x1"))
+b = int ( input ( " enter distance x2"))
+c = int ( input ( " enter a distance y1"))
+d = int ( input ( " input a distance Y2"))
+
+# distance b/w two point
+z = (a-b)**2 + ( c-d)**2
+print ("distance bw two point ",z)
